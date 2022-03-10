9fe0c486ebb4ac5280ad3f66c961327cac777d5d
Joseph Bulbulia <joseph.bulbulia@gmail.com>
2022-03-10 21:04:09 +1300

    updated to 2 waves

--- data/readat.R
+++ data/readat.R
@@ -3,6 +3,6 @@ library(tidyverse)
 
 df<-readRDS("/Users/joseph/The\ Virtues\ Project\ Dropbox/Joseph\ Bulbulia/00Bulbulia\ Pubs/2021/DATA/ldf.5")
 
-tidyverse::glimpse(df)
+dplyr::glimpse(df)
 
 saveRDS(df, here::here("data","df"))
